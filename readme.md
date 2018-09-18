2acbeb90659fba7e2485daaba15fb998eacecfd6
daniel40392 <daniel40392@gmail.com>
2018-09-18 19:30:21 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# GiantBomb-ify
+
+A chrome extension that will replace all <img> tags on your current page with one of 10 random [GiantBomb](http://www.giantbomb.com/) staff members.
+
+## Setup Instructions
+
+1. Clone repository locally with `git clone git@github.com:daniel40392/giantbomb-ify.git`
+
+2. Go to chrome://extensions and select 'load unpacked'
+
+3. Navigate to the downloaded giantbomb-ify directory and select OK
+
+4. Go to a webpage with images and click the chrome extension icon
---
+++ scripts/imageswap.js
@@ -0,0 +1,23 @@
+var chromeExtensionId = "iaimgpoecjggdpdlmlhhidciphppgfcf"
+
+var images = document.getElementsByTagName('img');
+
+var gb_images = [
+  "https://static.giantbomb.com/uploads/scale_medium/18/187968/2743737-yplezwp.jpg", //drew headset
+  "https://pbs.twimg.com/media/BpxlFaHCYAIQvvY.jpg", //brad mohawk
+  "https://static.giantbomb.com/uploads/original/7/72053/2671808-rorie.png", //rorie glasses
+  "http://static.giantbomb.com/uploads/original/13/139155/2536312-2379004-jeff_headshake.gif", //jeff disapproval
+  "http://static.giantbomb.com/uploads/original/17/179932/2783337-4310291077-prUNU.png", // dan creep
+  "https://static.giantbomb.com/uploads/original/2/23893/2455096-1935536898-10875.jpg", // vinny alpha protocol
+  "http://www.deepfreeze.it/img/j_headshots/alex_navarro.jpg", // alex smile
+  "https://magnettheater.com/oldImages/headshots/large_abbyrussell_2017.jpg", // abby face
+  "https://static.giantbomb.com/uploads/original/23/233047/2986879-2776158-0471975364-o4gps.gif", // jason approval
+  "https://78.media.tumblr.com/e0b2ba4e1fd8b1323b4e9c88477db345/tumblr_oshzt0nUPs1u7bbkho1_400.gif", // ben disapproval
+  `chrome-extension://${chromeExtensionId}/images/ryan.jpg` // local file example - ryan
+]
+
+for (var i = 0, l = images.length; i < l; i++) {
+  var index = Math.floor(Math.random() * Math.floor(i%gb_images.length+1))
+  images[i].src = gb_images[index];
+  var prev = i
+}
